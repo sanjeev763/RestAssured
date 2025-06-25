5adc8547ffdedf11640b874399a52ff9826fdf75
sanjeev763 <sanjeev76326510@gmail.com>
2025-06-26 00:41:19 +0530

    SerializationAndDeserialization

---
+++ src/test/java/serializationAndDeserialization/SerializationAndDeserialization.java
@@ -0,0 +1,42 @@
+package serializationAndDeserialization;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.testng.annotations.Test;
+
+//POJO --> JSON : Serialization
+//JSON --> POJO : Deserialization
+public class SerializationAndDeserialization {
+    @Test(priority=1)
+    void convertPOJOToJSON() throws JsonProcessingException {
+        //create java object using pojo class
+        Student student = new Student(); //At this point data is in the form of pojo
+        student.setName("sanjeev");
+        student.setLocation("India");
+        student.setPhone("123456");
+        String[] courses = {"c", "c++"};
+        student.setCoursesArr(courses);
+        //convert java object to json
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsondata = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(student);
+        System.out.println(jsondata);
+    }
+
+    @Test(priority=2)
+    void convertJSONToPOJO() throws JsonProcessingException {
+        String jsonData = "{\n" +
+                "  \"name\" : \"sanjeev\",\n" +
+                "  \"location\" : \"India\",\n" +
+                "  \"phone\" : \"123456\",\n" +
+                "  \"coursesArr\" : [ \"c\", \"c++\" ]\n" +
+                "}";
+        //convert json to java object
+        ObjectMapper objectMapper = new ObjectMapper();
+        Student std = objectMapper.readValue(jsonData, Student.class);
+        System.out.println(std.getLocation());
+        System.out.println(std.getName());
+        System.out.println(std.getPhone());
+        System.out.println(std.getCoursesArr()[0]);
+        System.out.println(std.getCoursesArr()[1]);
+    }
+}
---
+++ src/test/java/serializationAndDeserialization/Student.java
@@ -0,0 +1,44 @@
+package serializationAndDeserialization;
+
+
+public class Student {
+    private String name;
+    private String location;
+    private String phone;
+    private String[] coursesArr;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String[] getCoursesArr() {
+        return coursesArr;
+    }
+
+    public void setCoursesArr(String[] coursesArr) {
+        this.coursesArr = coursesArr;
+    }
+
+
+
+}
